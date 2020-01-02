7849f23a274bea0ca92ca427deb45166beb01611
Danil <danil.v.annenkov@gmail.com>
2020-01-02 12:55:10 +0100

    Docs

---
+++ .coqdeps.d
@@ -0,0 +1,14 @@
+Stlc/stlc.vo Stlc/stlc.glob Stlc/stlc.v.beautified: Stlc/stlc.v
+Stlc/stlc.vio: Stlc/stlc.v
+Stlc/OrdSet.vo Stlc/OrdSet.glob Stlc/OrdSet.v.beautified: Stlc/OrdSet.v
+Stlc/OrdSet.vio: Stlc/OrdSet.v
+Stlc/CustomTactics.vo Stlc/CustomTactics.glob Stlc/CustomTactics.v.beautified: Stlc/CustomTactics.v
+Stlc/CustomTactics.vio: Stlc/CustomTactics.v
+Stlc/Nom.vo Stlc/Nom.glob Stlc/Nom.v.beautified: Stlc/Nom.v Stlc/OrdSet.vo Stlc/CustomTactics.vo
+Stlc/Nom.vio: Stlc/Nom.v Stlc/OrdSet.vio Stlc/CustomTactics.vio
+Stlc/nomstlc.vo Stlc/nomstlc.glob Stlc/nomstlc.v.beautified: Stlc/nomstlc.v Stlc/CustomTactics.vo Stlc/Nom.vo Stlc/OrdSet.vo
+Stlc/nomstlc.vio: Stlc/nomstlc.v Stlc/CustomTactics.vio Stlc/Nom.vio Stlc/OrdSet.vio
+Stlc/stlc_bidir.vo Stlc/stlc_bidir.glob Stlc/stlc_bidir.v.beautified: Stlc/stlc_bidir.v Stlc/stlc.vo
+Stlc/stlc_bidir.vio: Stlc/stlc_bidir.v Stlc/stlc.vio
+Stlc/Goedel.vo Stlc/Goedel.glob Stlc/Goedel.v.beautified: Stlc/Goedel.v
+Stlc/Goedel.vio: Stlc/Goedel.v
