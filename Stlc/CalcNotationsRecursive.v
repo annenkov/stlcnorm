5d9280e68b1a8c7af2b524c1d2c11faf04d87d20
Danil <danil.v.annenkov@gmail.com>
2020-02-24 17:09:46 +0100

    Fixed a link

--- Stlc/CalcNotationsRecursive.v
+++ Stlc/CalcNotationsRecursive.v
@@ -1,7 +1,7 @@
 (** * Notation for reasoning using several transitive steps (recursive) *)
 
 Require Import Arith.
-(** Inspired by this: https://gist.github.com/geo2a/31381aeb345c789761504da1b5d42168 *)
+(** Inspired by this: https://gist.github.com/gallais/f046bcc2c348c5fed5e9 *)
 
 (** This notation is similar to [CalcNotations.v], but works for any number of transitive steps. However, it fails when one needs to use rewrites or, say ring, because we want to avoid writing the "middle points" when we use the notation. E.g. we write [_ = b by p] for all the steps excluding the first one. They can be ommitted because we know that the previous equation "end" coincudes with the "start" of the current one, since we build a chain of transitive steps. Usually, these "middle points" can be inferred by Coq, but if we use [ltac:()] to build terms witnessing the transitivity steps, the "middle points" might be not resolved yet and the tactics in the [ltac:()] call might fail.   *)
 
